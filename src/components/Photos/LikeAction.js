bec1840e544ca12983704d6fc2a96a944fa8183d
Petr Stratonnikov <pstratonni@gmail.com>
2021-06-17 14:10:32 +0200

    rew

--- src/components/Photos/LikeAction.js
+++ src/components/Photos/LikeAction.js
@@ -1,29 +1,37 @@
-import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { connect } from 'react-redux';
-import { addLikes } from '../../store/action/photos';
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { connect } from "react-redux";
+import { addLikes } from "../../store/action/photos";
 
-const LikeAction=({icons,activePerson,personId,addLike,classGreenRed,alfa,photoId })=>{
-return(
+const LikeAction = ({
+  icons,
+  activePerson,
+  personId,
+  addLike,
+  classGreenRed,
+  alfa,
+  photoId,
+}) => {
+  return (
     <FontAwesomeIcon
-                icon={icons}
-                className={`${classGreenRed} ${
-                  activePerson !== personId && activePerson ? " pointer" : ""
-                }`}
-                onClick={
-                  activePerson !== personId && activePerson
-                    ? () => addLike(photoId, alfa)
-                    : null
-                }
-              />
-)
-}
+      icon={icons}
+      className={`${classGreenRed} ${
+        activePerson !== personId && activePerson ? " pointer" : ""
+      }`}
+      onClick={
+        activePerson !== personId && activePerson
+          ? () => addLike(photoId, alfa)
+          : null
+      }
+    />
+  );
+};
 const mapStateToProps = (state) => {
-    return {
-      activePerson: state.persons.activePerson,
-    };
+  return {
+    activePerson: state.persons.activePerson,
   };
+};
 const mapDispatchToProps = (dispatch) => {
-    return { addLike: (photoId, alfa) => dispatch(addLikes(photoId, alfa)) };
-  };
-export default connect(mapStateToProps ,mapDispatchToProps) (LikeAction)
+  return { addLike: (photoId, alfa) => dispatch(addLikes(photoId, alfa)) };
+};
+export default connect(mapStateToProps, mapDispatchToProps)(LikeAction);
